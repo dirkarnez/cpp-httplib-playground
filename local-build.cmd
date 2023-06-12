048749efea3f97f99464186688b1a4fa541df68b
dirkarnez <smalldirkalex@gmail.com>
2023-06-12 23:22:15 +0800

    - init, wip

---
+++ local-build.cmd
@@ -0,0 +1,26 @@
+@REM run as Administrator
+@echo off
+cd /d %~dp0
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+set DOWNLOADS_DIR_LINUX=%DOWNLOADS_DIR:\=/%
+
+SET PATH=^
+%DOWNLOADS_DIR%\PortableGit\bin;^
+%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
+%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
+%DOWNLOADS_DIR%\cmake-3.26.1-windows-x86_64\bin;
+
+@REM set PATH=^
+@REM D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
+@REM D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
+@REM D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
+
+cmake.exe -G"MinGW Makefiles" ^
+-DCMAKE_BUILD_TYPE=Debug ^
+-Dhttplib_DIR="%DOWNLOADS_DIR_LINUX%/cpp-httplib-v0.12.6-mingw64-x86_64-posix-seh-rev0-8.1.0/lib/cmake/httplib" ^
+-DOpenSSL_DIR="%DOWNLOADS_DIR_LINUX%/openssl-v3.0.5-x86_64-posix-seh-rev0-8.1.0" ^
+-B./cmake-build &&^
+cd cmake-build &&^
+cmake --build . &&^
+echo "Successful build"
+pause
